f9d376e5b266b4f56ba83a8651f7490dc235d97e
QuangMinh0806 <lequangminh07072003@gmail.com>
2025-08-19 11:38:41 +0700

    1908_v2

--- BE/services/tai_khoan.js
+++ BE/services/tai_khoan.js
@@ -19,13 +19,40 @@ const findforLevel1 = async (user) => {
 
 
 const findforLevel2 = async (user) => {
-    console.log("User for Level 2:", user.hanh_dong);
-    const sql = `SELECT tk.*, pb.ten
+    const sql = `WITH RECURSIVE ThongTinDangNhap AS (
+                    SELECT 
+                        ttdn.id,
+                        ttdn.trang_thai,
+                        ttdn.ngay_thu_hoi,
+                        ttdn.ngay_cap,
+                        ts.ten_tai_san,
+                        ts.ten_nha_cung_cap,
+                        tk.id AS tai_khoan_id
+                    FROM thong_tin_dang_nhap_tai_san ttdn
+                    JOIN tai_san ts ON ts.id = ttdn.tai_san_id
+                    JOIN tai_khoan tk ON tk.id = ttdn.nguoi_nhan_id)
+
+
+
+                SELECT 	
+                    tk.*,
+                    pb.ten,
+                    JSONB_AGG(
+                        DISTINCT JSONB_BUILD_OBJECT(
+                                'thong_tin_dang_nhap_id', ttdn.id,
+                                'trang_thai', ttdn.trang_thai,
+                                'ngay_thu_hoi', ttdn.ngay_thu_hoi,
+                                'ngay_cap', ttdn.ngay_cap,
+                                'ten_tai_san', ttdn.ten_tai_san,
+                                'ten_nha_cung_cap', ttdn.ten_nha_cung_cap
+                            )
+                        ) AS thong_tin_dang_nhap
                 FROM tai_khoan tk
                 JOIN phong_ban pb ON tk.phong_ban_id = pb.id
-                WHERE tk.cap > ${user.cap}
-                AND pb.id = ${user.PhongBanId};`;
-    console.log("SQL Query:", sql);
+                JOIN ThongTinDangNhap ttdn ON ttdn.tai_khoan_id = tk.id
+                WHERE tk.cap > ${user.cap} AND pb.id = ${user.PhongBanId}
+                GROUP BY  tk.id, pb.ten;`;
+
     const results = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT });
     const value = {
             loai_hanh_dong: "Lấy danh sách người dùng cho cấp 2",
